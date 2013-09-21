96f40d44a2c39e4a66c63ceadcfb4a2f723c8703
Jourdan <jourdang@10ddb1bab56e.ant.amazon.com>
2013-09-20 18:57:07 -0700

    Sprucing up the front panel, nothing crazy.

--- src/com/banzz/lifecounter/activities/FrontMenuActivity.java
+++ src/com/banzz/lifecounter/activities/FrontMenuActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,6 +23,8 @@ public class FrontMenuActivity extends android.app.Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
         setContentView(R.layout.front_menu);
         
         Button mButton = (Button) findViewById(R.id.two_player_button);
